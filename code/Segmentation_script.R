be9fb803292f17ab4e99c6fcba9ea24c9c50555d
Paul Cabrisy <plc21@ic.ac.uk>
2022-03-11 10:31:59 +0000

    adding segmentation script

---
+++ code/Segmentation_script.R
@@ -0,0 +1,12 @@
+rm(list =ls())
+
+library(tidyverse)
+library(keras)
+library(tensorflow)
+#remotes::install_github("maju116/platypus")
+library(platypus)
+library(magick)
+library(png)
+library(jpeg)
+
+
