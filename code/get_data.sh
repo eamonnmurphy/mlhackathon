28d3a3d84a011a9b383964f8b42b8abe989bdf73
tashramsden <natasha.ramsden18@gmail.com>
2022-03-11 11:40:31 +0000

    Clear install requirements

--- code/get_data.sh
+++ code/get_data.sh
@@ -2,6 +2,11 @@
 # Inputs: $1 num images of each species
 # Example usage: bash get_data.sh 10
 
+# Requires ruby package nokogiri
+# Install: 
+# sudo apt install ruby
+# sudo gem install nokogiri
+
 for order in *.txt; do
     echo "Order: $order";
     mkdir -p ../data/${order%.*}  # make directory for images of this order
@@ -9,7 +14,6 @@ for order in *.txt; do
     while IFS= read -r line; do
         echo "Getting images of: $line"
         ./dwn_pics.rb -s $line -n $1 -f ../data/${order%.*} -o ../sandbox/metadata.txt -r small
-    done < "$order"  # filename
+    done < "$order"
 
 done
-
